77763bf89457ded32f43d21ec7adc72e2961842b
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-06-03 12:59:16 +0300

    exported secret key

---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY='kirui-yegon006'
+export MAIL_USERNAME='kibetkirui010@gmail.com'
+export MAIL_PASSWORD='Manchester44'
+export DATABASE_URL='postgresql+psycopg2://ian:5575@localhost/blogs'
+
+python3 manage.py runserver
